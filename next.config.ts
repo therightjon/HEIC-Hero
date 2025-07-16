e52a1587d93a0e19d56f01375a7b19a44b156a00
Jonathan Steen <therightjon@gmail.com>
2025-07-16 15:49:21 -0500

    Added correct Basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   /* config options here */
-  basePath: '/base',
+  basePath: '/heic-hero',
   typescript: {
     ignoreBuildErrors: true,
   },
